cabefcdda25ffa117352887876d3ea46d6ac9c59
林鈺錦 <abc1236762@outlook.com>
2019-05-13 20:13:48 +0800

    fix bugs

--- Adafruit_PCA9685/PCA9685.py
+++ Adafruit_PCA9685/PCA9685.py
@@ -23,48 +23,46 @@ import logging
 import time
 import math
 
-
 # Registers/etc:
-PCA9685_ADDRESS    = 0x40
-MODE1              = 0x00
-MODE2              = 0x01
-SUBADR1            = 0x02
-SUBADR2            = 0x03
-SUBADR3            = 0x04
-PRESCALE           = 0xFE
-LED0_ON_L          = 0x06
-LED0_ON_H          = 0x07
-LED0_OFF_L         = 0x08
-LED0_OFF_H         = 0x09
-ALL_LED_ON_L       = 0xFA
-ALL_LED_ON_H       = 0xFB
-ALL_LED_OFF_L      = 0xFC
-ALL_LED_OFF_H      = 0xFD
+PCA9685_ADDRESS = 0x40
+MODE1 = 0x00
+MODE2 = 0x01
+SUBADR1 = 0x02
+SUBADR2 = 0x03
+SUBADR3 = 0x04
+PRESCALE = 0xFE
+LED0_ON_L = 0x06
+LED0_ON_H = 0x07
+LED0_OFF_L = 0x08
+LED0_OFF_H = 0x09
+ALL_LED_ON_L = 0xFA
+ALL_LED_ON_H = 0xFB
+ALL_LED_OFF_L = 0xFC
+ALL_LED_OFF_H = 0xFD
 
 # Bits:
-RESTART            = 0x80
-SLEEP              = 0x10
-ALLCALL            = 0x01
-INVRT              = 0x10
-OUTDRV             = 0x04
-
+RESTART = 0x80
+SLEEP = 0x10
+ALLCALL = 0x01
+INVRT = 0x10
+OUTDRV = 0x04
 
 logger = logging.getLogger(__name__)
 
 
 def software_reset(i2c=None, **kwargs):
-    """Sends a software reset (SWRST) command to all servo drivers on the bus."""
-    # Setup I2C interface for device 0x00 to talk to all of them.
+    """Sends software reset (SWRST) command to all servo drivers on the bus."""
     if i2c is None:
         import Adafruit_GPIO.I2C as I2C
         i2c = I2C
-    self._device = i2c.get_i2c_device(0x00, **kwargs)
-    self._device.writeRaw8(0x06)  # SWRST
+    # Setup I2C interface for device 0x00 to talk to all of them.
+    device = i2c.get_i2c_device(0x00, **kwargs)
+    device.writeRaw8(0x06)  # SWRST
 
 
 class PCA9685(object):
     """PCA9685 PWM LED/servo controller."""
-
+    
     def __init__(self, address=PCA9685_ADDRESS, i2c=None, **kwargs):
         """Initialize the PCA9685."""
         # Setup I2C interface for the device.
@@ -80,32 +78,32 @@ class PCA9685(object):
         mode1 = mode1 & ~SLEEP  # wake up (reset sleep)
         self._device.write8(MODE1, mode1)
         time.sleep(0.005)  # wait for oscillator
-
+    
     def set_pwm_freq(self, freq_hz):
-        """Set the PWM frequency to the provided value in hertz."""
-        prescaleval = 25000000.0    # 25MHz
-        prescaleval /= 4096.0       # 12-bit
-        prescaleval /= float(freq_hz)
-        prescaleval -= 1.0
+        """Sets the PWM frequency to the provided value in hertz."""
+        pre_scale_val = 25000000.0  # 25MHz
+        pre_scale_val /= 4096.0  # 12-bit
+        pre_scale_val /= float(freq_hz)
+        pre_scale_val -= 1.0
         logger.debug('Setting PWM frequency to {0} Hz'.format(freq_hz))
-        logger.debug('Estimated pre-scale: {0}'.format(prescaleval))
-        prescale = int(math.floor(prescaleval + 0.5))
-        logger.debug('Final pre-scale: {0}'.format(prescale))
-        oldmode = self._device.readU8(MODE1);
-        newmode = (oldmode & 0x7F) | 0x10    # sleep
-        self._device.write8(MODE1, newmode)  # go to sleep
-        self._device.write8(PRESCALE, prescale)
-        self._device.write8(MODE1, oldmode)
+        logger.debug('Estimated pre-scale: {0}'.format(pre_scale_val))
+        pre_scale = int(math.floor(pre_scale_val + 0.5))
+        logger.debug('Final pre-scale: {0}'.format(pre_scale))
+        old_mode = self._device.readU8(MODE1)
+        new_mode = (old_mode & 0x7F) | 0x10  # sleep
+        self._device.write8(MODE1, new_mode)  # go to sleep
+        self._device.write8(PRESCALE, pre_scale)
+        self._device.write8(MODE1, old_mode)
         time.sleep(0.005)
-        self._device.write8(MODE1, oldmode | 0x80)
-
+        self._device.write8(MODE1, old_mode | 0x80)
+    
     def set_pwm(self, channel, on, off):
         """Sets a single PWM channel."""
-        self._device.write8(LED0_ON_L+4*channel, on & 0xFF)
-        self._device.write8(LED0_ON_H+4*channel, on >> 8)
-        self._device.write8(LED0_OFF_L+4*channel, off & 0xFF)
-        self._device.write8(LED0_OFF_H+4*channel, off >> 8)
-
+        self._device.write8(LED0_ON_L + 4 * channel, on & 0xFF)
+        self._device.write8(LED0_ON_H + 4 * channel, on >> 8)
+        self._device.write8(LED0_OFF_L + 4 * channel, off & 0xFF)
+        self._device.write8(LED0_OFF_H + 4 * channel, off >> 8)
+    
     def set_all_pwm(self, on, off):
         """Sets all PWM channels."""
         self._device.write8(ALL_LED_ON_L, on & 0xFF)
